933bb45b94eb89aabebf6d2b161234e931aca3ce
lululufr <lucas.lm.miller@gmail.com>
2023-12-30 12:55:15 +0100

    ADD - aller retour

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,61 @@
 use std::io::{self, Read, Write};
 use std::net::TcpStream;
+use std::sync::{Arc, Mutex};
+use std::thread;
 
-fn main() -> io::Result<()> {
-    let mut stream = TcpStream::connect("51.77.193.65:8080")?;
 
+fn co() -> std::io::Result<TcpStream> {
+    let stream =  TcpStream::connect("51.77.193.65:4242");
     println!("Connected to server!");
-
-    let mut buffer = [0; 512];
-
+    return stream;
+}
+fn emission(mut stream: TcpStream){
     loop {
         let message = get_user_input();
-        stream.write_all(message.as_bytes())?;
+        stream.write_all(message.as_bytes());
+    }
+}
+fn reception(mut stream: TcpStream) {
+    let mut buffer = [0; 512];
+    loop {
+        match stream.read(&mut buffer) {
+            Ok(bytes_read) => {
+                if bytes_read == 0 {
+                    // La connexion a été fermée
+                    break;
+                }
 
-        let bytes_read = stream.read(&mut buffer)?;
-        if bytes_read == 0 {
-            // La connexion a été fermée
-            break;
-        }
-
-        let response = String::from_utf8_lossy(&buffer[..bytes_read]);
-        println!("Received from Python: {}", response);
-
-        if response.to_lowercase().contains("quitter") {
-            break;
+                let response = String::from_utf8_lossy(&buffer[..bytes_read]);
+                println!("Received from Python: {}", response);
+            }
+            Err(err) => {
+                eprintln!("Error reading from socket: {}", err);
+                break;
+            }
         }
     }
+}
+
+
+
+fn main() -> io::Result<()> {
+    let mut connexion:TcpStream = co()?;
+    let mut connexion2:TcpStream = connexion.try_clone()?;
+
+
+    let thread_emission = thread::spawn(move|| {
+        emission(connexion);
+    });
+
+    let thread_reception = thread::spawn(move|| {
+        reception(connexion2);
+    });
+
+    thread_emission.join().expect("Error joining emission thread");
+    thread_reception.join().expect("Error joining reception thread");
 
     Ok(())
+
 }
 
 fn get_user_input() -> String {
